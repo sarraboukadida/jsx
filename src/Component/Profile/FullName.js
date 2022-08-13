ba6db91a7d1a67d51cde773700703f56945ba3b1
sarraboukadida <boukadida.sarah@gmail.com>
2022-08-14 01:12:42 +0200

    checkpoint jsx

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullName = () => {
+  return (
+    <div>
+        <h2>Nom et prenom: Boukadida sarra </h2>
+    </div>
+  )
+}
+
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import pic from './taswira.jpg'
+
+const ProfilePhoto = () => {
+  return (
+    <div className="Photo">
+        <h1>photo de profil</h1>
+        <img src={pic} alt=""/> 
+        
+    </div>
+  )
+}
+
+export default ProfilePhoto
